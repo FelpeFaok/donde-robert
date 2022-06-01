aaacf65dba5cc4970812c81c187d7880f930f3dc
KATHERINE CASTILLO <Katherine.Castillo@iprospect.com>
2022-06-01 15:54:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector("#toggle");
+const body = document.querySelector("body");
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("menu-active");
+});
