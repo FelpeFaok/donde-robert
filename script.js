fbbf89bf4a88ed94e84977aed8d8b6e7a5721fcc
FelpeFaok <felpe.faok@gmail.com>
2022-06-01 19:23:44 -0400

    first commit: boton y lo rompi :)

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+// activar menu
 const toggle = document.querySelector("#toggle");
 const body = document.querySelector("body");
 
 toggle.addEventListener("click", () => {
   body.classList.toggle("menu-active");
 });
+
+// // boton texto cambio
+
+// const cambioTextoBtn = document.querySelector("#cambioTexto");
+// const toggleText = () => {
+//   cambioTextoBtn.addEventListener("click", () => {
+//     const parrafo = document.querySelector("#parrafo");
+//     if (parrafo.innerHTML == "cerramos") {
+//       parrafo.innerHTML = "hola soy el chef Robert";
+//     } else {
+//       parrafo.innerHTML = "cerramos";
+//     }
+//   });
+// };
+
+// toggleText();
+
+// // cambio color texto
+
+// const cambioColorBtn = document.querySelector("#cambioTexto");
+// const toggleColor = () => {
+//   cambioColorBtn.addEventListener("click", () => {
+//     const parrafo = document.querySelector("#parrafo");
+//     parrafo.style.color = "black"
+//     if (parrafo.style.color == "black") {
+//       parrafo.style.color = "white";
+//     } else if (parrafo.style.color === "white") {
+//       parrafo.style.color = "red";
+//     }
+//   });
+// };
+
+// toggleColor();
